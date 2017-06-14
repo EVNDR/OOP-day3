9830b67a159180b9210d3e3ad3d8185a550468ea
EVNDR <osweree@gmail.com>
2017-06-14 18:48:13 +0300

    Add files via upload

---
+++ OOP,day3.py.py
@@ -0,0 +1,21 @@
+class games(object):
+    def _init_(self,football,javelin,chess,members,coach,):
+        self.members = members
+        self.coach = coach
+
+class football(games):        
+    def _init_(self,balls,*args)
+        self.balls = balls
+        games._init_(self,*args)
+
+class javelin(games): 
+    def _init_(self,javelins)
+        self.javelins = javelins
+        games._init_(self,*args)
+
+class chess(games):
+    def _init_(self,chess)
+        self.chess = chess
+        games._init_(self.*args)
+
+
